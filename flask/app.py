ef8d2a7d6705d3c6a71687d3119486902ab65f9c
siddhartha-12 <siddhartharaju12@gmail.com>
2021-08-08 19:30:46 -0400

    Setting up flask server

--- flask/app.py
+++ flask/app.py
@@ -1,1 +1,11 @@
-api_key = "api key"
+import requests
+from flask import Flask,Flask, request, render_template,jsonify
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=97071d9cfaf6a74c61ab998ed28f6259'
+    return render_template('index.html')
