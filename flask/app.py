a8c37450a04d338fbf5fc6f940d0009a0b30ff61
siddhartha-12 <siddhartharaju12@gmail.com>
2021-08-08 21:01:56 -0400

    Adding post method for city search

--- flask/app.py
+++ flask/app.py
@@ -7,23 +7,34 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///weather.db'
 database = SQLAlchemy(app)
 
-class city(database.Model):
+class City(database.Model):
     id = database.Column(database.Integer, primary_key=True)
     name = database.Column(database.Integer, nullable=False)
 
 
-@app.route('/')
+@app.route('/', methods=['GET','POST'])
 def index():
+    if request.method == 'POST':
+        new_city = request.form.get('city')
+
+        if new_city:
+            nc_object = City(name=new_city)
+            database.session.add(nc_object)
+            database.session.commit()
+    city_list = City.query.all()
     url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=97071d9cfaf6a74c61ab998ed28f6259'
-    city = "Boston"
-    req = requests.get(url.format(city)).json()
-    print (req)
 
-    weather_data = {
-        'city' : city, 
-        'tempreture' : req['main']['temp'],
-        'description' : req['weather'][0]['description'],
-        'icon' : req['weather'][0]['icon']
-    }
+    weather_city_list = []
 
-    return render_template('index.html', weather=weather_data)
+    for city in city_list:
+        req = requests.get(url.format(city.name)).json()
+    
+        weather_data = {
+            'city' : city.name, 
+            'tempreture' : req['main']['temp'],
+            'description' : req['weather'][0]['description'],
+            'icon' : req['weather'][0]['icon']
+        }
+        weather_city_list.append(weather_data)
+
+    return render_template('index.html', weathers=weather_city_list)
