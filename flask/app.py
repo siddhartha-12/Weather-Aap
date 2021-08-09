9281df7147dc940f6451942a37f1361312e26363
siddhartha-12 <siddhartharaju12@gmail.com>
2021-08-08 20:11:03 -0400

    Setting up open weather API call

--- flask/app.py
+++ flask/app.py
@@ -8,4 +8,15 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=97071d9cfaf6a74c61ab998ed28f6259'
-    return render_template('index.html')
+    city = "Boston"
+    req = requests.get(url.format(city)).json()
+    print (req)
+
+    weather_data = {
+        'city' : city, 
+        'tempreture' : req['main']['temp'],
+        'description' : req['weather'][0]['description'],
+        'icon' : req['weather'][0]['icon']
+    }
+
+    return render_template('index.html', weather=weather_data)
