42b82b3eb6232893ff343134f5eac70d6fbdac2d
siddhartha-12 <siddhartharaju12@gmail.com>
2021-08-08 23:58:37 -0400

    Adding error and success notification

--- flask/app.py
+++ flask/app.py
@@ -1,4 +1,4 @@
-from flask.helpers import url_for
+from flask.helpers import flash, url_for
 import requests
 from flask import Flask,Flask, request, render_template,jsonify,redirect
 from flask_sqlalchemy import SQLAlchemy
@@ -7,6 +7,7 @@ import os
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///weather.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'testkey'
 database = SQLAlchemy(app)
 
 class City(database.Model):
@@ -17,15 +18,12 @@ class City(database.Model):
 @app.route('/')
 def index():
     city_list = City.query.all()
-    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=97071d9cfaf6a74c61ab998ed28f6259'
-
     weather_city_list = []
 
     for city in city_list:
-        req = requests.get(url.format(city.name)).json()
-    
+        req = getCityData(city.name)
         weather_data = {
-            'city' : city.name, 
+            'city' : city, 
             'tempreture' : req['main']['temp'],
             'description' : req['weather'][0]['description'],
             'icon' : req['weather'][0]['icon']
@@ -34,18 +32,37 @@ def index():
 
     return render_template('index.html', weathers=weather_city_list)
 
+def getCityData(city):
+    url = f'http://api.openweathermap.org/data/2.5/weather?q={ city }&units=metric&appid=97071d9cfaf6a74c61ab998ed28f6259'
+    req = requests.get(url).json()
+    return req
+
 
 
 @app.route('/', methods=['POST'])
 def index_post():
-
+    flash_message = ''
     new_city = request.form.get('city')
-
+    
     if new_city:
-        nc_object = City(name=new_city)
-        database.session.add(nc_object)
-        database.session.commit()
+        check_city  = City.query.filter_by(name=new_city).first()
+        if not check_city:
+            valid_city_check = getCityData(new_city)
+            if valid_city_check['cod'] == 200:
+                nc_object = City(name=new_city)
+                database.session.add(nc_object)
+                database.session.commit()
+            else:
+                flash_message = 'City not found'
+        else:
+            flash_message = 'Message already on dashboard'
+    if flash_message:
+        flash(flash_message,'alert')
+    else:
+        flash('Congratulations')
+
     return redirect(url_for('index'))
-    return render_template('index.html', weathers=weather_city_list)
+    
+
 
 
