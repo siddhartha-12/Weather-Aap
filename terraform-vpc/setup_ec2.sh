9f9993fa9e683a56b145cbb89561092b8d9035b6
siddhartha-12 <siddhartharaju12@gmail.com>
2021-08-15 18:23:55 -0400

    Fixing installation error

--- terraform-vpc/setup_ec2.sh
+++ terraform-vpc/setup_ec2.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
-sudo apt-get install ansible
+sudo apt-get install ansible -y
 git clone https://github.com/siddhartha-12/Weather-App.git
+pwd
 cd Weather-App/Ansible
 ansible-playbook applicationSetup.yml 
-rm -rf Weather-App
+
