977d581cbd16397ee8299bddddda8e411054c633
siddhartha-12 <siddhartharaju12@gmail.com>
2021-08-16 01:18:47 -0400

    Fixed ingress port issue

--- terraform-vpc/main.tf
+++ terraform-vpc/main.tf
@@ -12,13 +12,21 @@ resource "aws_security_group" "Project-SG" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-  ingress {
-    from_port   = 5000
-    to_port     = 5000
+
+    ingress {
+    from_port   = 8080
+    to_port     = 8080
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-  
+
+      ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   ingress {
     from_port   = 22
     to_port     = 22
